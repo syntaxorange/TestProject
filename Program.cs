52aaf4d16125f0f70dc637ff828279fb359bae3c
syntaxorange <syntaxorange@gmail.com>
2023-06-13 21:47:11 +0300

    rate of subscriptions

--- Program.cs
+++ Program.cs
@@ -1,32 +1,25 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(0, 11);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+if (daysUntilExpiration <= 10)
 {
-  if ((roll1 == roll2) && (roll2 == roll3))
+  if (daysUntilExpiration > 1 && daysUntilExpiration <= 5)
   {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+  }
+  else if (daysUntilExpiration == 1)
+  {
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires within a day!\nRenew now and save 20%!");
+  }
+  else if (daysUntilExpiration == 0)
+  {
+    Console.WriteLine("Your subscription has expired.");
   }
   else
   {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
   }
-}
-
-if (total >= 15)
-{
-  Console.WriteLine("You win!");
-}
-else
-{
-  Console.WriteLine("Sorry, you lose.");
 }
