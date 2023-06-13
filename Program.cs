a2071593c2f6a3b6f939062e6cebed08e5f67ba0
syntaxorange <syntaxorange@gmail.com>
2023-06-13 21:52:19 +0300

    rate of subscriptions: refactor

--- Program.cs
+++ Program.cs
@@ -2,24 +2,26 @@
 int daysUntilExpiration = random.Next(0, 11);
 int discountPercentage = 0;
 
-if (daysUntilExpiration <= 10)
+if (daysUntilExpiration == 0)
 {
-  if (daysUntilExpiration > 1 && daysUntilExpiration <= 5)
-  {
-    discountPercentage = 10;
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
-  }
-  else if (daysUntilExpiration == 1)
-  {
-    discountPercentage = 20;
-    Console.WriteLine($"Your subscription expires within a day!\nRenew now and save 20%!");
-  }
-  else if (daysUntilExpiration == 0)
-  {
-    Console.WriteLine("Your subscription has expired.");
-  }
-  else
-  {
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-  }
+  Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+  Console.WriteLine($"Your subscription expires within a day!");
+  discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+  Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+  discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+  Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+  Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
