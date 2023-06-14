99973434a3f289f8081cae2d03602aa2089b2680
syntaxorange <syntaxorange@gmail.com>
2023-06-14 20:20:50 +0300

    Array

--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(0, 11);
-int discountPercentage = 0;
+﻿/*
+string[] fraudulentOrderIDs = new string[3];
 
-if (daysUntilExpiration == 0)
-{
-  Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-  Console.WriteLine($"Your subscription expires within a day!");
-  discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-  Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-  discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-  Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+*/
 
-if (discountPercentage > 0)
-{
-  Console.WriteLine($"Renew now and save {discountPercentage}%.");
-}
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" }; 
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign first: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
