abd037b3f2abc5bdb99c6e7ab605bfa8245b0f9f
syntaxorange <syntaxorange@gmail.com>
2023-06-15 15:49:55 +0300

    code comments

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
-﻿string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-foreach (string orderID in orderIDs)
+﻿/*
+string firstName = "Bob";
+int widgetsPurchased = 7;
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
+Random random = new Random();
+string[] orderIDs = new String[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
 {
-  if (orderID.StartsWith("B"))
-  {
-    Console.WriteLine(orderID);
-  }
+  // Get a random value that equates to ASCIII letters A through E
+  int prefixValue = random.Next(65, 70);
+  // Convert the random value into a char, then a string
+  string prefix = Convert.ToChar(prefixValue).ToString();
+  // Create a random number, pad with zeroes
+  string suffix = random.Next(1, 1000).ToString("000");
+  // Combine the prefix and suffix together, then assign to current OrderID
+  orderIDs[i] = prefix + suffix;
+}
+// Print out each orderIDs
+foreach (var orderID in orderIDs)
+{
+  Console.WriteLine(orderID);
 }
